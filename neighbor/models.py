7b3daf7d3021c7d18a4ee057956eb04c3fee10b4
Julia Agasaro <agasarojulia@gmail.com>
2019-11-01 14:43:46 +0200

    Added models

--- neighbor/models.py
+++ neighbor/models.py
@@ -1,3 +1,60 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Location(models.Model):
+    locations = (
+        ('Kigali', 'Kimironko'),
+        ('Kigali', 'Kacyiru'),
+        ('Kigali', 'Kicukiro'),
+        ('Kigali', 'Kimihurura'),
+        ('Kigali', 'Gacuriro'),
+        ('Kigali', 'Nyamirambo'),
+        ('Kigali', 'Gikondo'),
+        ('Kigali', 'Kiyovu'),
+        ('Kigali', 'Kanombe'),
+        ('Kigali', 'Kibagabaga'),
+        ('Kigali', 'Gisozi'),
+              
+    )
+    name = models.CharField(max_length=65, choices=locations)
+
+
+
+    def save_loc(self):
+        self.save()
+
+    def delete_loc(self):
+        self.delete()
+
+
+    def __str__(self):
+        return self.name
+
+
+class Hood(models.Model):
+    name = models.CharField(max_length=50)
+    occupants = models.CharField(max_length=50)
+    location = models.ForeignKey(Location)
+    
+
+
+
+
+
+
+class Profile(models.Model):
+    profile_pic = models.ImageField(upload_to = 'profile/',blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    bio = models.CharField(max_length = 255,null = True)
+    full_name = models.CharField(max_length=255, null=True)
+    hood = models.ForeignKey(Hood,null=True)
+
+    def __str__(self):
+        return self.user.username
+
+    def save_profile(self):
+        self.save()
+
+
+
+    
--- neighbor/views.py
+++ neighbor/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http  import HttpResponse
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+@login_required(login_url='/accounts/login/')
+def profile(request, username):
+
+    profile = User.objects.get(username=username)
+    print(profile.id)
+    try:
+        profile_details = Profile.get_by_id(profile.id)
+    except:
+        pass
+    user = request.user
+    profile = User.objects.get(username=username)
+    title = f'@{profile.username} '
+
+    return render(request, 'profile.html', locals()) 
